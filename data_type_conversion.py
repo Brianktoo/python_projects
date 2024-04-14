ca8edd99d2452f3da4c43fd1b82b893ee51f2392
Brian Kipkosgei <brianktoo1998@gmail.com>
2024-04-14 09:30:34 +0300

    data type convertion and concatination

---
+++ data_type_conversion.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+x= 1000
+y=0.14
+z="brian your  change is "
+
+converted_x =str(x)
+converted_y =str(y)
+
+result = z + converted_x + ' and ' + converted_y + " cents "
+print(result)
