7ed6e4e4c842e0b652b4a0507e6e7fe775d26745
Brian Kipkosgei <brianktoo1998@gmail.com>
2024-04-14 10:28:26 +0300

    use of input and string formating to print message

---
+++ string_formating.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+firstName= input('Enter your first name:')
+lastName =input('Enter your last name:')                                        
+
+firstNameLower=firstName.lower()
+
+lastNameLower = lastName.lower()
+
+
+
+print(f"Hello {firstNameLower.title()} {lastNameLower.title()} Welcome to Python Programming.")
