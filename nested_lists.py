1aab98faf71c02bddd2b2ecbee77f0cffd5e1c57
Brian Kipkosgei <brianktoo1998@gmail.com>
2024-04-14 12:21:15 +0300

    nested lists containing 3 inner lists

---
+++ nested_lists.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+#nested list declaration and initialization
+studentScores= [[30,60,70],[40,80,90],[70,90,40]]
+
+#average score of student2
+student1_average=sum(studentScores[0])/3
+print(f"student1 average score is: {student1_average}")
+
+#average score of student2
+student2_average =sum(studentScores[1])/3
+print(f"student2 average score is: {student2_average}")
+
+#average score of student 3
+student3_average =sum(studentScores[2])/3
+print(f"student3 average score is: {student3_average}")
+
+
