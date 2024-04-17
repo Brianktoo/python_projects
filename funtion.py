bd2c17c051b7846241b89f7810ebad7ee8931e3f
Brian Kipkosgei <brianktoo1998@gmail.com>
2024-04-17 22:27:13 +0300

    branch test

---
+++ funtion.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+i=20
+if(i<15):
+    print('i is smaller than 15')
+else:
+    print('i is greater that 15')
---
+++ login.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+x= input('Enter your passwor to login:')
+if x=="Tyondo@98":
+    print('successfully login')
+else:
+    print('incorrect password')
